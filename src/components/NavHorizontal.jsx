c562530453bc4a0f201f1f32f71f265e2c18aa5d
Clément Théraud <theraud.clement@gmail.com>
2022-07-04 11:39:04 +0200

    navs pages h+v start
    start functions
    coms

--- src/components/NavHorizontal.jsx
+++ src/components/NavHorizontal.jsx
@@ -1,3 +1,31 @@
+import
+import
+
+
+/**
+ * Function NavHorizontal returns a div for the current user
+ * @param {array} currentUser / properties contains userId state + setUserId 
+ * @returns a div with the logo of Sportsee + a horizontal navigation
+ */
 function NavHorizontal ({ currentUser}) {
-    
+    //declare currentUser (array)
+    const [userId, setUserId] = currentUser;
+    const ids = [12, 18];
+
+    //return a div for navigation horizontal
+    return(
+        <div className="NavH">
+        //logo + onclick to switch demo
+        </div>
+
+        <div className="NavHBtn">
+            //buttons nav
+            <span className="NavHBtn_menu">Accueil</span>
+            <span className="NavHBtn_menu">Accueil</span>
+            <span className="NavHBtn_menu">Accueil</span>
+            <span className="NavHBtn_menu">Accueil</span>
+
+        </div>
+    )
+
 }
--- src/components/NavVertical.jsx
+++ src/components/NavVertical.jsx
@@ -1,3 +1,14 @@
-function NavVertical ({ currentUser}) {
+import
+import
+import
+import
+
+/**
+ * function NavVertical returns a div who contains vertical navigation
+ * 4 buttons are present, each represent a picture of a sport
+ * a div regroups the 4 buttons
+ * the copyright is inside a div
+ */
+function NavVertical () {
     
 }
