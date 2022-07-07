58a19ac41b6545e213c8253fe6eb95c41882cd66
Clément Théraud <theraud.clement@gmail.com>
2022-07-07 10:52:52 +0200

    data axios

---
+++ src/app/getData.js
@@ -0,0 +1,13 @@
+import {
+    USER_MAIN_DATA,
+    USER_ACTIVITY,
+    USER_AVERAGE_SESSIONS,
+    USER_PERFORMANCE
+} from "../assets/mockedData/data"
+
+import PropTypes from "prop-types"
+import axios from "axios"
+
+async function getData (requestTarget, userId) {
+    
+}
